ab161bd88dbbdf853d74dd5457153b04d584ed2c
Arga Aji Prasetyo <argaaji120@gmail.com>
2024-11-20 22:23:15 +0700

    crud categories

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import authMiddleware from "../middlewares/auth.middleware";
 import authController from "../controllers/auth.controller";
+import categoryController from "../controllers/category.controller";
 
 const router = express.Router();
 
@@ -9,5 +10,12 @@ router.post("/auth/login", authController.login);
 router.post("/auth/register", authController.register);
 router.post("/auth/me", authMiddleware, authController.me);
 router.put("/auth/update-profile", authMiddleware, authController.updateProfile);
+
+// Categories
+router.get("/categories", categoryController.findAll);
+router.post("/categories", categoryController.create);
+router.get("/categories/:id", categoryController.findOne);
+router.put("/categories/:id", categoryController.update);
+router.delete("/categories/:id", categoryController.delete);
 
 export default router;
